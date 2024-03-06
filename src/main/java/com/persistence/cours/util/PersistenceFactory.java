d065406ccb0a80de5c8b1fa0037975318409e5d4
François Caremoli <francois.caremoli@wanadoo.fr>
2024-03-06 13:32:32 +0100

    premier commit

---
+++ src/main/java/com/persistence/cours/util/PersistenceFactory.java
@@ -0,0 +1,33 @@
+package com.persistence.cours.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+/**
+ * Singleton de création d'entityManager
+ */
+public class PersistenceFactory {
+
+	private static final String PERSISTENCE_UNIT_NAME = "com.bigcorp.persistence";
+	public static PersistenceFactory INSTANCE = new PersistenceFactory();
+	private boolean init;
+	private EntityManagerFactory emf;
+
+	private PersistenceFactory() {
+	}
+
+	/**
+	 * Récupère un entityManager.
+	 * Crée l'entityManagerFactory au besoin.
+	 * @return
+	 */
+	public synchronized EntityManager getEntityManager() {
+		if (!this.init) {
+			this.emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+			this.init = true;
+		}
+		return this.emf.createEntityManager();
+	}
+
+}
